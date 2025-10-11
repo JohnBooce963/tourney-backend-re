aaf83ddad9acc67475f6e9c2d417c9762e9e7a10
JohnBooce <64050538@kmitl.ac.th>
2025-10-11 13:44:18 +0700

    no more sse add env + ably

--- backend/src/service/ably-ws-service.ts
+++ backend/src/service/ably-ws-service.ts
@@ -1,6 +1,6 @@
 import Ably from "ably";
 
-const ably = new Ably.Rest(process.env.ABLY_API_KEY!);
+const ably = new Ably.Rest({ key: process.env.ABLY_API_KEY! });;
 
 export const publishLobbies = (data: any) => {
   ably.channels.get("lobbies").publish("lobbies", data);
