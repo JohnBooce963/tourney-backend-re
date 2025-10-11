b84d089abfb5c01872e90fe6cc85737303d8fab7
JohnBooce <64050538@kmitl.ac.th>
2025-10-11 08:12:38 +0700

    change lobby-room component(add loadLobby)

---
+++ backend/src/app/api/lobby/[id]/route.ts
@@ -0,0 +1,19 @@
+import { lobbyService } from "@/service/lobby-service";
+import { withCors, corsOptionsResponse } from "@/libs/cor";
+
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
+
+    try{
+        const res = lobbyService.getLobbyInfo(id);
+        return withCors(req, res, {status: 200})
+    }catch (err) {
+        return withCors(req, { error: "Failed to get lobby info" }, { status: 500 });
+    }
+    
+}
+
+export async function OPTIONS(req: Request) {
+  return corsOptionsResponse(req);
+}
