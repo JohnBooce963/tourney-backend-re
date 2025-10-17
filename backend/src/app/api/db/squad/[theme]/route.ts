0deccb6c9ab21c55969be5d3c9509d65d1594c63
JohnBooce <64050538@kmitl.ac.th>
2025-10-17 13:28:10 +0700

    fix cold start on frontend and backend

--- backend/src/app/api/db/squad/[theme]/route.ts
+++ backend/src/app/api/db/squad/[theme]/route.ts
@@ -1,11 +1,13 @@
 import { corsOptionsResponse, withCors } from "@/libs/cor";
 import { dbService } from "@/service/db-service";
 
-export async function GET(req: Request,  context: { params: Promise<{ theme: number }> }) {
+export async function GET(req: Request,  context: { params: Promise<{ theme: string  }> }) {
     const { theme } = await context.params;
 
+    const themeId = Number(theme);
+
     try{
-        const res = await dbService.getSpecificTheme(theme);
+        const res = await dbService.getSpecificTheme(themeId);
         return withCors(req, res, {status: 200})
     }catch (err) {
         return withCors(req, { error: "Failed to get lobby info" }, { status: 500 });
